1a4ccaf0c36fd080253da429da7505f3746306a6
Mika Vesalainen <mikki@vesalainen.name>
2021-03-26 19:33:03 +0200

    Updated runme file

--- runme.sh
+++ runme.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Build image
 TAG=embtbdex
-
 docker build --tag $TAG docker/.
 
-docker run --rm -it -v $PWD:/data $TAG make -C /data
+# Run image
+case $1 in
+    clean)
+	CMD="make -C /data clean"
+	;;
+
+    sh|bash)
+	CMD="sh"
+	;;
+
+    *)
+	CMD="make -C /data"
+	;;
+esac
+docker run --rm -it -v $PWD:/data $TAG $CMD
+
